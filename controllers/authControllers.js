3253e2f089f019abc988d3fec186138cee5f3c10
Nazar-Natin <114873430+Nazar-Natin@users.noreply.github.com>
2024-03-24 22:42:42 +0100

    Update JS. Fifth task.

---
+++ controllers/authControllers.js
@@ -0,0 +1,99 @@
+import catchAsync from "../helpers/catchAsync.js";
+import {
+  signup,
+  verify,
+  reverify,
+  login,
+  updateAvatar,
+} from "../services/usersServices.js";
+import { User } from "../models/userModel.js";
+import { Email } from "../services/emailService.js";
+
+export const registerUser = catchAsync(async (req, res) => {
+  const newUser = await signup(req.body);
+  const { email, verificationToken } = newUser;
+
+  try {
+    const url = `${req.protocol}://${req.get(
+      "host"
+    )}/users/verify/${verificationToken}`;
+
+    await new Email(newUser, url).sendVerification();
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(201).json({
+    user: {
+      email,
+      subscription: "starter",
+    },
+  });
+});
+
+export const verifyUser = catchAsync(async (req, res) => {
+  const { verificationToken } = req.params;
+  console.log(req.params);
+
+  await verify(verificationToken);
+
+  res.status(200).json({
+    message: "Verification successful",
+  });
+});
+
+export const reverifyUser = catchAsync(async (req, res) => {
+  const user = await reverify(req.body);
+
+  const { verificationToken } = user;
+
+  try {
+    const url = `${req.protocol}://${req.get(
+      "host"
+    )}/users/verify/${verificationToken}`;
+
+    await new Email(user, url).sendVerification();
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(200).json({
+    message: "Verification email sent",
+  });
+});
+
+export const loginUser = catchAsync(async (req, res) => {
+  const { email, token } = await login(req.body);
+
+  res.status(200).json({
+    token,
+    user: {
+      email,
+      subscription: "starter",
+    },
+  });
+});
+
+export const getCurrentUser = (req, res) => {
+  const { email, subscription } = req.user;
+  res.status(200).json({
+    email,
+    subscription,
+  });
+};
+
+export const logoutUser = catchAsync(async (req, res) => {
+  const { _id } = req.user;
+
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).send();
+});
+
+export const updateUserAvatar = catchAsync(async (req, res) => {
+  const { avatarURL } = await updateAvatar(req.user, req.file);
+
+  res.status(200).json({
+    avatarURL,
+  });
+});
---
+++ services/usersService.js
@@ -0,0 +1,68 @@
+import { User } from "../models/userModel.js";
+import { signToken } from "../services/jwtService.js";
+import HttpError from "../helpers/HttpError.js";
+import { ImageService } from "../services/imageService.js";
+import { v4 } from "uuid";
+
+async function signup(userData) {
+  userData.verificationToken = v4();
+
+  const newUser = await User.create(userData);
+
+  return newUser;
+}
+
+async function verify(verificationToken) {
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) throw HttpError(404, "User not found");
+
+  user.verificationToken = null;
+  user.verify = true;
+
+  return user.save();
+}
+
+async function reverify({ email }) {
+  const user = await User.findOne({ email });
+
+  if (user.verify) throw HttpError(400, "Verification has already been passed");
+
+  return user;
+}
+
+async function login({ email, password }) {
+  const user = await User.findOne({ email });
+
+  if (!user) throw HttpError(401, "Email or password is wrong");
+
+  if (!user.verify) throw HttpError(401, "User is not verified");
+
+  const isPasswordValid = await user.checkPassword(password, user.password);
+
+  if (!isPasswordValid) throw HttpError(401, "Email or password is wrong");
+
+  const token = signToken(user.id);
+
+  await User.findByIdAndUpdate(user.id, { token });
+
+  return { email, token };
+}
+
+async function updateAvatar(user, file) {
+  if (file) {
+    user.avatarURL = await ImageService.saveImage(
+      file,
+      {
+        maxFileSize: 2,
+        width: 250,
+        height: 250,
+      },
+      "avatars"
+    );
+  }
+
+  return user.save();
+}
+
+export { signup, verify, reverify, login, updateAvatar };
