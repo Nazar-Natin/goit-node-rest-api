656d8ee61a632b7244906aa97e5cd4dd397323a4
Nazar-Natin <114873430+Nazar-Natin@users.noreply.github.com>
2024-03-24 16:13:53 +0100

    Update JS. Added try/catch to controller

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -10,43 +10,59 @@ import {
 import catchAsync from "../helpers/catchAsync.js";
 
 export const getAllContacts = catchAsync(async (req, res) => {
-  const contacts = await listContacts();
-
-  res.status(200).json(contacts);
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export const getOneContact = catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const contact = await getContactById(id);
-
-  res.status(200).json(contact);
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export const deleteContact = catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const removedContact = await removeContact(id);
-
-  res.status(200).json(removedContact);
+  try {
+    const { id } = req.params;
+    const removedContact = await removeContact(id);
+    res.status(200).json(removedContact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export const createContact = catchAsync(async (req, res) => {
-  const newContact = await addContact(req.body);
-
-  res.status(201).json(newContact);
+  try {
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export const updateOneContact = catchAsync(async (req, res) => {
-  const { id } = req.params;
-
-  const updatedContact = await updateContact(id, req.body);
-
-  res.status(200).json(updatedContact);
+  try {
+    const { id } = req.params;
+    const updatedContact = await updateContact(id, req.body);
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export const updateStatus = catchAsync(async (req, res) => {
-  const { id } = req.params;
-
-  const updatedContact = await updateStatusContact(id, req.body);
-
-  res.status(200).json(updatedContact);
+  try {
+    const { id } = req.params;
+    const updatedContact = await updateStatusContact(id, req.body);
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
