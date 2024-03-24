cf4af82e2f3678a1f8813e24721540979e5e5a18
Nazar-Natin <114873430+Nazar-Natin@users.noreply.github.com>
2024-03-24 17:16:31 +0100

    Update JS. Fourth task.

---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+import { model, Schema } from "mongoose";
+import { compare, genSalt, hash } from "bcrypt";
+import crypto from "crypto";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+    avatarURL: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const emailHash = crypto.createHash("md5").update(this.email).digest("hex");
+
+    this.avatarURL = `https://www.gravatar.com/avatar/${emailHash}.jpg?d=robohash`;
+  }
+
+  if (!this.isModified("password")) return next();
+
+  const salt = await genSalt(10);
+  this.password = await hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.checkPassword = (candidate, passwordHash) =>
+  compare(candidate, passwordHash);
+
+export const User = model("User", userSchema);
---
+++ routes/authRouters.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+  logoutUser,
+  updateUserAvatar,
+} from "../controllers/authControllers.js";
+import {
+  checkRegisterData,
+  checkLoginData,
+  protect,
+} from "../middlewares/authMiddleware.js";
+import { uploadAvatar } from "../middlewares/userMiddleware.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", checkRegisterData, registerUser);
+authRouter.post("/login", checkLoginData, loginUser);
+authRouter.get("/current", protect, getCurrentUser);
+authRouter.post("/logout", protect, logoutUser);
+authRouter.patch("/avatars", protect, uploadAvatar, updateUserAvatar);
+
+export default authRouter;
