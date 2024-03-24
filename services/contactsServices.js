a1b1c881424d1fa50b8f7ea8e2eeffcf8efa4240
Nazar-Natin <114873430+Nazar-Natin@users.noreply.github.com>
2024-03-24 10:49:25 +0100

    Update JS. Second task.

---
+++ services/contactsServices.js
@@ -0,0 +1,54 @@
+import { Contact } from "../models/contactModel.js";
+
+async function listContacts() {
+  const contacts = await Contact.find();
+
+  return contacts;
+}
+
+async function getContactById(id) {
+  const contact = Contact.findById(id);
+
+  return contact;
+}
+
+async function removeContact(id) {
+  const removedContact = Contact.findByIdAndDelete(id);
+
+  return removedContact;
+}
+
+async function addContact(contactData) {
+  const newContact = await Contact.create(contactData);
+
+  return newContact;
+}
+
+async function updateContact(id, contactData) {
+  const updatedContact = Contact.findByIdAndUpdate({ _id: id }, contactData, {
+    new: true,
+  });
+
+  return updatedContact;
+}
+
+async function updateStatusContact(id, contactStatus) {
+  const updatedStatusContact = Contact.findByIdAndUpdate(
+    { _id: id },
+    contactStatus,
+    {
+      new: true,
+    }
+  );
+
+  return updatedStatusContact;
+}
+
+export {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact,
+};
